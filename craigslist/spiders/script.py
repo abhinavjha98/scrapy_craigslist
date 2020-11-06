a4763f7af6af9cec60b619c6f903aba0dade6df9
abhinavjha98 <abhinavjha98ald@gmail.com>
2020-11-06 19:37:20 +0530

    Added the correction

--- craigslist/spiders/script.py
+++ craigslist/spiders/script.py
@@ -19,9 +19,9 @@ class DmozItem(scrapy.Item):
 	Email = scrapy.Field()
 class DmozSpider(scrapy.Spider):
 	name = "craig"
-	page_numbers = 0
+	page_numbers = 120
 	start_urls = [
-	'https://losangeles.craigslist.org/d/retail-wholesale/search/ret?s=0'
+	'https://minneapolis.craigslist.org/d/services/search/bbb'
 	]
 	def __init__(self):
 		self.driver = webdriver.Chrome('E:/PROJECT/scrapy_python/craigslist/craigslist/chromedriver.exe')
@@ -29,45 +29,60 @@ class DmozSpider(scrapy.Spider):
 		links = response.css('a.result-title').xpath("@href").extract()
 		for link in links:
 			yield scrapy.Request(link, callback=self.parse_attr)
-		next_page = "https://losangeles.craigslist.org/d/retail-wholesale/search/ret?s="+str(DmozSpider.page_numbers)
-		if DmozSpider.page_numbers<=500:
-			DmozSpider.page_numbers +=120+
-			yield response.follow(next_page,callback=self.parse)
+		# next_page = "https://minneapolis.craigslist.org/search/bbb?s="+str(DmozSpider.page_numbers)
+		# if DmozSpider.page_numbers<=6000:
+		# 	DmozSpider.page_numbers +=120
+		# 	yield response.follow(next_page,callback=self.parse)
+		next_page = response.css('a.button.next').xpath("@href").extract()
+		if next_page:
+			next_href = next_page[0]
+			next_page_url = 'https://minneapolis.craigslist.org' + next_href
+			request = scrapy.Request(url=next_page_url)
+			yield request
 
 	def parse_attr(self, response):
-		self.driver.get(response.url)
-		try:
-			next = self.driver.find_elements_by_css_selector("button.reply-button")
-			next[0].click()
-			time.sleep(2)
-		except NoSuchElementException:
-			next=""
+		# self.driver.get(response.url)
+		# try:
+		# 	next = self.driver.find_elements_by_css_selector("button.reply-button")
+		# 	next[0].click()
+		# 	time.sleep(2)
+		# except NoSuchElementException:
+		# 	next=""
 
 		item = DmozItem()
 
-		try:
-			next1 = self.driver.find_element_by_css_selector("div aside ul li p")
-			Name = next1.text
-		except NoSuchElementException:
-			Name = ""
+		# try:
+		# 	next1 = self.driver.find_element_by_css_selector("div aside ul li p")
+		# 	Name = next1.text
+		# except NoSuchElementException:
+		# 	Name = ""
 
-		try:
-			next2 = self.driver.find_element_by_css_selector("a.mailapp")
-			Email = next2.text
-		except NoSuchElementException:
-			Email = ""
-		if "@" in Name:
-			Name=""	
+		# try:
+		# 	next2 = self.driver.find_element_by_css_selector("a.mailapp")
+		# 	Email = next2.text
+		# except NoSuchElementException:
+		# 	Email = ""
+		# if "@" in Name:
+		# 	Name=""	
 		Link = response.url
 		Title = response.css('h1 span span::text').extract()
 		Location = response.css('h1 span small::text').extract()
 		
 		ll = response.css('p span b::text').extract()
-		Compensation = ll[0]
-		Employment_type = ll[1]
+		if not ll:
+			return
+		else:
+			Compensation = ll[0]
+			try:
+				Employment_type = ll[1] 
+			except IndexError:
+				Employment_type = ""
 		Address = response.css('div.mapaddress::text').extract()
-		Location[0] = Location[0].replace(" (","")
-		Location[0] = Location[0].replace(")","")
+		try:
+			Location[0] = Location[0].replace(" (","")
+			Location[0] = Location[0].replace(")","")
+		except IndexError:
+			Location=""	
 		Description = response.xpath("//section[@id='postingbody']/descendant::text()").extract()
 		res=[]
 		
@@ -89,7 +104,7 @@ class DmozSpider(scrapy.Spider):
 		# 	text = text.rstrip("\n")
 		# 	text_list=text_list+text
 		# text_list = text_list.rstrip("\n")
-		item['Name'] = Name
+		item['Name'] = ""
 		item['Title'] = Title
 		item['Location'] = Address
 		item['Compensation'] = Compensation
@@ -97,6 +112,6 @@ class DmozSpider(scrapy.Spider):
 		item['Address'] = Location
 		item['Description'] = text_list
 		item['Link'] = Link
-		item['Email'] = Email
+		item['Email'] = ""
 		return item
 	
